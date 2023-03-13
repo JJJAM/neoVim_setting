83f1287a0f19682eb83b67562bd86033abfa4863
郑在忙 <topzzjj@163.com>
2023-03-13 13:43:09 +0800

    keymap

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -6,3 +6,6 @@ keymap.set("i", "jk", "<ESC>")
 
 
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- 格式化json
+keymap.set("n", "<leader>jj", "<ESC>:%!python3 -m json.tool<CR>")
--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -39,6 +39,21 @@ return require('packer').startup(function(use)
   use("christoomey/vim-tmux-navigator")
   use "nvim-treesitter/nvim-treesitter"
   use "p00f/nvim-ts-rainbow"
+
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",  -- 这个相当于mason.nvim和lspconfig的桥梁
+    "neovim/nvim-lspconfig"
+  }
+
+    -- 自动补全
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "L3MON4D3/LuaSnip" -- snippets引擎，不装这个自动补全会出问题
+  use "saadparwaiz1/cmp_luasnip"
+  use "rafamadriz/friendly-snippets"
+  use "hrsh7th/cmp-path" -- 文件路径
+
   if packer_bootstrap then
     require('packer').sync()
   end
